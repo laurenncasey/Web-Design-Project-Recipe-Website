22691122d54f42d9e43291c9be25f8703e052b4e
laurencasey <caseyln5088@uwec.edu>
2022-12-04 14:47:29 -0600

    modified:   babka.php 	modified:   bananabread.php 	modified:   chocchipcookie.php 	modified:   cinrolls.php 	modified:   css/about.css 	modified:   css/style.css 	new file:   css/testStyle.css 	deleted:    fonts/Anthony Hunter.otf 	deleted:    fonts/Noteworthy.ttc 	new file:   fonts/adelia.otf 	modified:   index.html 	modified:   lemon.php 	modified:   lemonpound.php 	modified:   pbcookie.php 	modified:   pecanpie.php 	modified:   pumpkinpie.php 	modified:   recipes.html 	modified:   shrimpboil.php 	modified:   yeastRolls.php

--- pumpkinpie.php
+++ pumpkinpie.php
@@ -3,32 +3,48 @@
 
 
 echo "<!DOCTYPE html>
-<html> <head> <link rel='stylesheet' href='css/style.css'> <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'> <meta charset='UTF-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-<link rel='icon' type='image/x-icon' href='images/export2White.png'>            
-            <title>Casey's Pumpkin Pie Recipe</title> </head>
+<html>
+    <head>
+    <link rel='stylesheet' href='css/testStyle.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link rel='icon' type='image/x-icon' href='images/export2White.png'>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Recipe</title>
+</head>
+<body class = 'bodyTest'>
 
 
-<body>
-    <body class='mainbody'>
     <div class='header'>
-    <a class='nav' href='about.html'>About</a>
-    <a class='homenav' href='index.html'><img class='logo' src='images/export 2.png' alt='The Casey Journals Home Page'></a>
-    <a class = 'nav' href='recipes.html'>Recipes</a>
+        <a class='nav' href='about.html'>About</a>
+        <a class='homenav' href='index.html'><img class='logo' src='images/export 2.png' alt='The Casey Journals Home Page'></a>
+        <a class = 'nav' href='recipes.html'>Recipes</a>
+    </div>
+
+
+    <div class = 'constantImages'>
+    <img class = 'imgHeader' src='../WebsiteProject/images/ball.jpeg' alt = 'ball of dough'>
+    <img class = 'imgHeader' src='../WebsiteProject/images/babkaBread.jpeg' alt = 'loaf of babka'>
+    <img class = 'imgHeader' src='../WebsiteProject/images/lemonpound.jpg' alt = 'lemon pound cake'>
+    <img class = 'imgHeader' src='../WebsiteProject/images/macarons.jpg' alt = 'french macarons'>
+    <img class = 'imgHeader' src='../WebsiteProject/images/sourDough.jpg' alt = 'sour dough baked bread'>
 </div>
 
-    <div class = 'totalPage'>
 
-        <div class='content2'>
-            <img class = 'food2' src='../WebsiteProject/images/IMG_0029.HEIC' alt='Pumpkin Pie'>
+    <div class = 'recipenameholder'>
+    <h1 class='title'>Pumpkin Pie</h1>
+    </div>
 
+    <div class = 'book'>
+
+        <div class = 'left'>
         </div>
 
-        <h1 class = 'recipeNameh1'>Pumpkin Pie</h1>
-
-        <div class='book'>
-            <div class='ingr'>
-            <h2 class='label'>Ingredients</h2>
-            <p class='para'>";
+    <div class = 'middle'>
+    <h2 class = 'label'>Ingredients</h2>
+    <div class='divIngr'>
+    <p class='ingr'>";
 
             
                         require_once('sql_conn.php');
@@ -46,18 +62,12 @@ echo "<!DOCTYPE html>
                         }
             
 
-            echo "</p>
-
-
-
-
-
-
-            </div>
-            <div class='instr'>
-                <h2 class='label'>Directions</h2>
-                <p>";
-
+                        echo "</p>
+                        </div>
+                        
+                            <h2 class = 'label'>Directions</h2>
+                            <p class='dir'>";
+            
             
             require_once('sql_conn.php');
 
@@ -77,36 +87,35 @@ echo "<!DOCTYPE html>
                 
                 
 
+           
             echo "</p>
-
-
-
-                </div>
             </div>
-
-
-        </div>
-    </div>
-    </body>
-
-    <form class = 'formclass' method='post' target='_blank' action='submitReview.php'>
-    <h1 class='formHeader'>Would you like to leave a review?</h1>
-    <input class='textbox' type='text' name='review'><br/><br/>
-    <input type='submit'>
-</form>
-
-        <div class='footer'>
-            <a class='icons' href = 'https://www.linkedin.com/in/alaina-casey-5229a317a' target='_blank'><i class='fa fa-linkedin'></i></a>
-            <a class='icons' href='https://instagram.com/thecaseyjournals?igshid=YmMyMTA2M2Y=' target='_blank'><i class='fa fa-instagram'></i></a>
-            <a class='icons' href='' target='_blank'><i class='fa fa-youtube'></i></a>
-        </div>
-</body>
-</html>";
+        
+            <div class = 'right'>
+            </div>
+        
+            </div>
+            <div class='separator'></div>
+        
+        
+           
+            <form class = 'formclass' method='post' target='_blank' action='submitReview.php'>
+                <h1 class='formHeader'>Would you like to leave a review?</h1>
+                <input class='textbox' type='text' name='review'><br/><br/>
+                <input class = 'submitbtn' type='submit'>
+            </form>
+        
+            <div class='footer'>
+                <a class='icons' href = 'https://www.linkedin.com/in/alaina-casey-5229a317a' target='_blank'><i class='fa fa-linkedin'></i></a>
+                <a class='icons' href='https://instagram.com/thecaseyjournals?igshid=YmMyMTA2M2Y=' target='_blank'><i class='fa fa-instagram'></i></a>
+                <a class='icons' href='' target='_blank'><i class='fa fa-youtube'></i></a>
+            </div>
+        
+        </body>
+        </html>";
 
 
 mysqli_close($dbc);
 ?>
-
-
 
 
